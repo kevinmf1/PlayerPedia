beac92675690611f8fb48c17e6b338ed417419ae
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-10-18 21:25:20 +0700

    feat: add empty viewmodel

---
+++ app/src/main/java/com/vinz/playerpedia/activity/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.vinz.playerpedia.activity.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/vinz/playerpedia/activity/login/LoginViewModel.kt
@@ -0,0 +1,6 @@
+package com.vinz.playerpedia.activity.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+}
