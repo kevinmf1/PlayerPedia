6ba4301c385480f5717237f38bfc48c6377bff42
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-10-19 23:35:33 +0700

    feat: add PlayerRepository

---
+++ app/src/main/java/com/vinz/playerpedia/data/local/repository/PlayerRepository.kt
@@ -0,0 +1,25 @@
+package com.vinz.playerpedia.data.local.repository
+
+import com.vinz.playerpedia.data.local.datasource.PlayerDataSource
+import com.vinz.playerpedia.data.local.entity.PlayerEntity
+
+class PlayerRepository(
+    private val dataSource: PlayerDataSource
+) {
+
+    fun insertPlayer(player: PlayerEntity) {
+        dataSource.insertPlayer(player)
+    }
+
+    fun deletePlayer(player: PlayerEntity) {
+        dataSource.deletePlayer(player)
+    }
+
+    fun updatePlayer(player: PlayerEntity) {
+        dataSource.updatePlayer(player)
+    }
+
+    fun getAllPlayer() = dataSource.getAllPlayer()
+
+    fun getPlayerById(playerId: Int) = dataSource.getPlayerById(playerId)
+}
