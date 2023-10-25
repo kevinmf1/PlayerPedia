fd96de0d3bf6340f85ea43326a52c26a2fb0fdee
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-10-25 13:36:46 +0700

    feat: implement injector

---
+++ app/src/main/java/com/vinz/playerpedia/core/di/UserViewModelFactory.kt
@@ -0,0 +1,36 @@
+package com.vinz.playerpedia.core.di
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.vinz.playerpedia.activity.login.LoginViewModel
+import com.vinz.playerpedia.activity.register.RegisterViewModel
+import com.vinz.playerpedia.core.domain.usecase.UserUseCase
+
+class UserViewModelFactory private constructor(private val userUseCase: UserUseCase) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+        @Volatile
+        private var instance: UserViewModelFactory? = null
+
+        fun getInstance(context: Context): UserViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: UserViewModelFactory(
+                    Injection.provideUserUseCase(context)
+                )
+            }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        when {
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(userUseCase) as T
+            }
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(userUseCase) as T
+            }
+            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
+        }
+}
