2175dc99544c0c7536c81f4548c67a939c9d8961
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-10-25 13:38:01 +0700

    feat: add dao (get data) for room

--- app/src/main/java/com/vinz/playerpedia/core/data/source/local/room/PlayerDao.kt
+++ app/src/main/java/com/vinz/playerpedia/core/data/source/local/room/PlayerDao.kt
@@ -1,13 +1,13 @@
-package com.vinz.playerpedia.data.local.room
+package com.vinz.playerpedia.core.data.source.local.room
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.vinz.playerpedia.data.local.entity.PlayerEntity
-import kotlinx.coroutines.flow.Flow
+import com.vinz.playerpedia.core.data.source.local.entity.PlayerEntity
 
 @Dao
 interface PlayerDao {
@@ -22,9 +22,9 @@ interface PlayerDao {
     fun updatePlayer(player: PlayerEntity)
 
     @Query("SELECT * FROM player ORDER BY player_name ASC")
-    fun getAllPlayer(): Flow<List<PlayerEntity>>
+    fun getAllPlayer(): LiveData<List<PlayerEntity>>
 
     @Query("SELECT * FROM player WHERE id = :playerId")
-    fun getPlayerById(playerId: Int): Flow<PlayerEntity>
+    fun getPlayerById(playerId: Int): LiveData<PlayerEntity>
 
 }
--- app/src/main/java/com/vinz/playerpedia/core/data/source/local/room/UserDao.kt
+++ app/src/main/java/com/vinz/playerpedia/core/data/source/local/room/UserDao.kt
@@ -1,13 +1,15 @@
-package com.vinz.playerpedia.data.local.room
+package com.vinz.playerpedia.core.data.source.local.room
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.vinz.playerpedia.data.local.entity.UserEntity
+import com.vinz.playerpedia.core.data.source.local.entity.UserEntity
 
+@Dao
 interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -21,4 +23,11 @@ interface UserDao {
 
     @Query("SELECT * FROM user WHERE id = :userId")
     fun getUserById(userId: Int): LiveData<UserEntity>
+
+    @Query("SELECT * FROM user WHERE email = :email AND password = :password")
+    fun getUserByEmailAndPassword(email: String, password: String): LiveData<UserEntity?>
+
+    @Query("SELECT * FROM user WHERE email = :email")
+    fun getUserByEmail(email: String): LiveData<UserEntity?>
+
 }
