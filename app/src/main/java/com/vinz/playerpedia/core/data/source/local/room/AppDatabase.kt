d135389036db08931ae1387fecabdb254bf09284
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-11-10 22:46:13 +0700

    chore: deleted all about playerEntity

--- app/src/main/java/com/vinz/playerpedia/core/data/source/local/room/AppDatabase.kt
+++ app/src/main/java/com/vinz/playerpedia/core/data/source/local/room/AppDatabase.kt
@@ -5,15 +5,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.vinz.playerpedia.core.data.source.local.entity.PlayerEntity
 import com.vinz.playerpedia.core.data.source.local.entity.UserEntity
 import com.vinz.playerpedia.core.utils.FileConverter
 
-@Database(entities = [PlayerEntity::class, UserEntity::class], version = 1, exportSchema = false)
+@Database(entities = [UserEntity::class], version = 3, exportSchema = false)
 @TypeConverters(FileConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun playerDao(): PlayerDao
     abstract fun userDao(): UserDao
 
     companion object {
