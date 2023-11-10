bb5ec31415198e1c2a058f633886e7d3b8def3c8
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-11-10 19:58:24 +0700

    feat: migrate from local data to remote

--- app/src/main/java/com/vinz/playerpedia/activity/home/HomeViewModel.kt
+++ app/src/main/java/com/vinz/playerpedia/activity/home/HomeViewModel.kt
@@ -1,12 +1,14 @@
 package com.vinz.playerpedia.activity.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.vinz.playerpedia.core.domain.model.Player
 import com.vinz.playerpedia.core.domain.usecase.PlayerUseCase
+import kotlinx.coroutines.Dispatchers
 
 class HomeViewModel(private val playerUseCase: PlayerUseCase) : ViewModel() {
 
-    val player = playerUseCase.getAllPlayer()
+    val player = playerUseCase.getAllRemotePlayer().asLiveData(Dispatchers.IO)
 
     fun deletePlayer(player: Player) {
         playerUseCase.deletePlayer(player)
