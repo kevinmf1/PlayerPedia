35bad9af7a8d8ae1b6e54d4037f67373f420f7a3
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-11-10 19:49:09 +0700

    feat: add get data from remote

--- app/src/main/java/com/vinz/playerpedia/core/domain/usecase/PlayerUseCase.kt
+++ app/src/main/java/com/vinz/playerpedia/core/domain/usecase/PlayerUseCase.kt
@@ -2,8 +2,14 @@ package com.vinz.playerpedia.core.domain.usecase
 
 import androidx.lifecycle.LiveData
 import com.vinz.playerpedia.core.domain.model.Player
+import com.vinz.playerpedia.core.domain.model.PlayerRemote
+import com.vinz.playerpedia.core.utils.ResultWrapper
+import kotlinx.coroutines.flow.Flow
 
 interface PlayerUseCase {
+
+    fun getAllRemotePlayer(): Flow<ResultWrapper<List<PlayerRemote>>>
+
     fun insertPlayer(player: Player)
 
     fun deletePlayer(player: Player)
