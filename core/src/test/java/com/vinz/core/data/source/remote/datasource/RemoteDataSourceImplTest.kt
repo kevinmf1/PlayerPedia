5ab1f9f938ab25e98bb279735e6291bf784f244a
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-11-20 00:11:57 +0700

    feat: add unit test

---
+++ core/src/test/java/com/vinz/core/data/source/remote/datasource/RemoteDataSourceImplTest.kt
@@ -0,0 +1,38 @@
+package com.vinz.core.data.source.remote.datasource
+
+import com.vinz.core.data.source.remote.network.APIService
+import com.vinz.core.data.source.remote.response.PlayerResponse
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class RemoteDataSourceImplTest {
+
+    @MockK
+    lateinit var service: APIService
+
+    private lateinit var dataSource: RemoteDataSource
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        dataSource = RemoteDataSourceImpl(service)
+    }
+
+    @Test
+    fun getPlayers() {
+        runTest {
+            val mockResponse = mockk<PlayerResponse>()
+            coEvery { service.getPlayers() } returns mockResponse
+            val response = dataSource.getPlayers()
+            coVerify { service.getPlayers() }
+            assertEquals(response, mockResponse)
+        }
+    }
+}
