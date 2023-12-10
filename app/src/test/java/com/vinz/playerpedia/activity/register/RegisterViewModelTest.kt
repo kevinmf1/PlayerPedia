bf762b8190eefbec30061f1124a8016eeae23d3e
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-12-10 23:39:51 +0700

    chore: add unit test for registerviewmodel

--- app/src/test/java/com/vinz/playerpedia/activity/register/RegisterViewModelTest.kt
+++ app/src/test/java/com/vinz/playerpedia/activity/register/RegisterViewModelTest.kt
@@ -10,6 +10,7 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -53,6 +54,44 @@ class RegisterViewModelTest {
     }
 
     @Test
+    fun `getUserByEmailAndPassword returns null user`() = runTest {
+        val email = "test@gmail.com"
+        val password = "password"
+
+        coEvery { userUseCase.getUserByEmailAndPassword(email, password) } returns flowOf(null)
+
+        val result = registerViewModel.getUserByEmailAndPassword(email, password)
+
+        result.test {
+            assertEquals(null, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `getUserByEmailAndPassword loading`() = runTest {
+        val email = "test@gmail.com"
+        val password = "password"
+        val user = User(
+            1,
+            email = email,
+            password = "password",
+            name = "Test User",
+            username = "testuser",
+            phone = "08123456789"
+        )
+
+        coEvery { userUseCase.getUserByEmailAndPassword(email, password) } returns flowOf(user)
+
+        val result = registerViewModel.getUserByEmailAndPassword(email, password)
+
+        result.test {
+            Assert.assertNotNull(awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
     fun `getUserByEmail returns expected user`() = runTest {
         val user = User(1, email = "test@gmail.com", password = "password", name = "Test User", username = "testuser", phone = "08123456789")
         val email = "test@gmail.com"
@@ -63,6 +102,42 @@ class RegisterViewModelTest {
 
         result.test {
             assertEquals(user, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `getUserByEmail returns null user`() = runTest {
+        val email = "test@gmail.com"
+
+        coEvery { userUseCase.getUserByEmail(email) } returns flowOf(null)
+
+        val result = registerViewModel.getUserByEmail(email)
+
+        result.test {
+            Assert.assertEquals(null, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `getUserByEmail loading`() = runTest {
+        val email = "test@gmail.com"
+        val user = User(
+            1,
+            email = email,
+            password = "password",
+            name = "Test User",
+            username = "testuser",
+            phone = "08123456789"
+        )
+
+        coEvery { userUseCase.getUserByEmail(email) } returns flowOf(user)
+
+        val result = registerViewModel.getUserByEmail(email)
+
+        result.test {
+            Assert.assertNotNull(awaitItem())
             awaitComplete()
         }
     }
